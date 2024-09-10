41d211b7ca797195348a7ae7ea04e83b31d7b95c
Dan Carter <djcarter85@gmail.com>
2024-09-10 21:00:41 +0100

    feat: improve nav links

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,11 +1,17 @@
+import cx from "classix";
+import { Link, useLocation } from "react-router-dom";
+
 const NavLink = ({ text, href }: { text: string; href: string }) => {
+  const { pathname } = useLocation();
+  const isActive = pathname === href;
+
   return (
-    <a
-      href={href}
-      className="font-display text-3xl text-primary-800 hover:text-secondary-600 lg:text-4xl"
+    <Link
+      to={href}
+      className={cx("font-display text-3xl text-primary-800 hover:text-secondary-600 lg:text-4xl", isActive && "text-secondary-600")}
     >
       {text}
-    </a>
+    </Link>
   );
 };
 
