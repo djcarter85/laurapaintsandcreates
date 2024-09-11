6cce371a0cf816a3c026089855f67c420aada84d
Dan Carter <djcarter85@gmail.com>
2024-09-11 11:13:11 +0100

    feat: change a few sizes

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -8,7 +8,10 @@ const NavLink = ({ text, href }: { text: string; href: string }) => {
   return (
     <Link
       to={href}
-      className={cx("font-display text-3xl text-primary-800 hover:text-secondary-600 lg:text-4xl", isActive && "text-secondary-600")}
+      className={cx(
+        "font-display text-2xl text-primary-800 hover:text-secondary-600 md:text-3xl lg:text-4xl",
+        isActive && "text-secondary-600",
+      )}
     >
       {text}
     </Link>
@@ -17,7 +20,7 @@ const NavLink = ({ text, href }: { text: string; href: string }) => {
 
 const Nav = () => {
   return (
-    <nav className="my-4 flex flex-row justify-center gap-4 lg:flex-col lg:justify-start">
+    <nav className="my-4 flex flex-row justify-center gap-x-4 gap-y-4 md:gap-x-6 lg:flex-col lg:justify-start lg:px-6 lg:my-6">
       <NavLink text="Portfolio" href="/" />
       <NavLink text="About" href="/about" />
       <NavLink text="Shop" href="/shop" />
