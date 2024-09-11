ae1156e0cb11cf4491e62906ecaf07b5ae766af2
Dan Carter <djcarter85@gmail.com>
2024-09-11 18:57:27 +0100

    feat: add link from logo to home page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import { Container } from "./container";
 import { Nav } from "./nav";
@@ -6,8 +7,10 @@ const Header = () => {
   return (
     <div className="bg-gradient-to-r from-primary-600 to-primary-500 py-4">
       <Container>
-        <div className="flex flex-col gap-4 md:flex-row justify-between items-center">
-          <img src={logo} className="my-4" />
+        <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+          <Link to="/">
+            <img src={logo} className="my-4" />
+          </Link>
           <Nav />
         </div>
       </Container>
