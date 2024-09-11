068f5bbc2e46e1e1ff51196b5749b89aefe011ef
Dan Carter <djcarter85@gmail.com>
2024-09-11 12:58:07 +0100

    feat: add some basic content to some of the pages

--- src/pages/about-page.tsx
+++ src/pages/about-page.tsx
@@ -1,5 +1,14 @@
+import { Para } from "../components/para";
+
 const AboutPage = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <Para>I'm Laura. I'm an illustrator based in Cambridgeshire, UK.</Para>
+      <Para>more info about me</Para>
+      <Para>even more info</Para>
+      <Para>maybe a picture?</Para>
+    </>
+  );
 };
 
 export { AboutPage };
--- src/pages/contact-page.tsx
+++ src/pages/contact-page.tsx
@@ -1,5 +1,15 @@
+import { Para } from "../components/para";
+
 const ContactPage = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Para>
+        If you'd like to get in touch with me, please use one of the links
+        below.
+      </Para>
+      <Para>link link link</Para>
+    </>
+  );
 };
 
 export { ContactPage };
