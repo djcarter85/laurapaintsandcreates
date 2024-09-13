c19299d84df65d1d43ab496b3bc9278a44dc5399
Dan Carter <djcarter85@gmail.com>
2024-09-13 18:02:55 +0100

    feat: add some images to portfolio page

--- src/pages/portfolio-page.tsx
+++ src/pages/portfolio-page.tsx
@@ -1,5 +1,29 @@
+import hares from "../assets/portfolio/hares.jpg";
+import otters from "../assets/portfolio/otters.jpg";
+import birthdayPheasants from "../assets/portfolio/birthday-pheasants.jpg";
+import newZealand from "../assets/portfolio/new-zealand.jpg";
+import stoat from "../assets/portfolio/stoat.jpg";
+import foxes from "../assets/portfolio/foxes.jpg";
+
+const PortfolioImage = ({ src }: { src: string }) => {
+  return (
+    <div className="aspect-square">
+      <img src={src} className="h-full w-full object-cover" />
+    </div>
+  );
+};
+
 const PortfolioPage = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <PortfolioImage src={hares} />
+      <PortfolioImage src={otters} />
+      <PortfolioImage src={birthdayPheasants} />
+      <PortfolioImage src={newZealand} />
+      <PortfolioImage src={stoat} />
+      <PortfolioImage src={foxes} />
+    </div>
+  );
 };
 
 export { PortfolioPage };
