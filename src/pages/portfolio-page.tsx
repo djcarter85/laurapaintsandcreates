ee9384f5f84be8e89261cab143b7531abea0ecf2
Dan Carter <djcarter85@gmail.com>
2024-09-14 09:15:03 +0100

    feat: add hover effect to categories

--- src/pages/portfolio-page.tsx
+++ src/pages/portfolio-page.tsx
@@ -4,18 +4,26 @@ import coast from "../assets/portfolio/coast.jpg";
 
 const Category = ({ imgSrc, title }: { imgSrc: string; title: string }) => {
   return (
-    <div className="flex flex-col gap-4 rounded-xl bg-primary-200 p-4">
-      <div className="aspect-square rounded-xl">
-        <img src={imgSrc} className="h-full w-full rounded-lg object-cover" />
+    <a
+      className="group mx-auto flex flex-col gap-3 items-center justify-center"
+      href="#"
+    >
+      <div className="aspect-square overflow-hidden rounded-xl">
+        <img
+          src={imgSrc}
+          className="h-full w-full object-cover transition-transform group-hover:scale-110"
+        />
       </div>
-      <div className="text-center text-2xl">{title}</div>
-    </div>
+      <div className="font-display text-5xl transition-transform group-hover:scale-90">
+        {title}
+      </div>
+    </a>
   );
 };
 
 const PortfolioPage = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 my-8">
       <Category imgSrc={hares} title="Wildlife" />
       <Category imgSrc={coast} title="Landscapes" />
       <Category imgSrc={newZealand} title="Portraits" />
